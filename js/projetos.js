8433d52b25e61665e7f540c172c2fab662c5f0b7
Maycon Padilha <maycon.jordan5000@gmail.com>
2024-09-20 18:49:37 -0300

    melhorias na responsividade

--- js/projetos.js
+++ js/projetos.js
@@ -84,6 +84,28 @@ function gerarListaProjetos(itens) {
             delay: 5000,
             disableOnInteraction: false,
         },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+
+            1200:{
+                slidesPerView: 3,
+                spaceBetween: 30
+            }
+        }
     });
 
     if (swiperWrapper.children.length <= 3) { 
